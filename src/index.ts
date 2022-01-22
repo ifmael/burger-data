9e792c1a57791bceba552f316432c22888536f9d
ismael Rodriguez <ismael.rm.1986@gmail.com>
2022-01-22 14:50:41 +0100

    generate dist  with restaurantdocument

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { useAppData } from "./hooks/useAppData";
 export { useRestaurantData } from "./hooks/useRestaurantData";
 
 export * from "./types";
+
+export { RestaurantDocument, RestaurantQueryResult } from "./graphql/models";
