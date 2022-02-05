a4c81040c4b299dba02dc8a4ab84beb296d84ee7
ismael Rodriguez <ismael.rm.1986@gmail.com>
2022-02-05 12:40:00 +0100

    fix: :bug: remove restaurant id literal and remove unnecessary useEffect

--- src/hooks/useAppData.ts
+++ src/hooks/useAppData.ts
@@ -1,49 +1,48 @@
-import { useEffect, useState } from "react";
-
 // Graphql
 import { useAppDataQuery } from "../graphql/models";
 
 // Utils
 import {
-  convertCategoryApp,
-  convertBurgerApp,
-  convertItemApp,
-  convertOptionApp,
-  convertRestaurantApp,
-  convertSaladApp,
-  convertSandwichApp,
-  convertSideApp,
-  convertSimpleModel,
-  convertIngredientApp,
+    convertCategoryApp,
+    convertBurgerApp,
+    convertItemApp,
+    convertOptionApp,
+    convertRestaurantApp,
+    convertSaladApp,
+    convertSandwichApp,
+    convertSideApp,
+    convertSimpleModel,
+    convertIngredientApp,
 } from "./Global.functions";
 
 // Types
 import { AppData } from "../types";
 
 export const useAppData = () => {
-  const { loading, error, data } = useAppDataQuery();
-  const [appData, setAppData] = useState<AppData>();
+    const { loading, error, data } = useAppDataQuery();
+    let appData: AppData | undefined;
 
-  useEffect(() => {
     try {
-      if (loading || !!error || !data) return;
-      setAppData({
-        beverages: convertSimpleModel(data.beverages),
-        desserts: convertSimpleModel(data.desserts),
-        salads: convertSaladApp(data.salads),
-        sides: convertSideApp(data.sides),
-        sandwiches: convertSandwichApp(data.sandwiches),
-        burgers: convertBurgerApp(data.burgers),
-        items: convertItemApp(data.items),
-        options: convertOptionApp(data.options),
-        restaurants: convertRestaurantApp(data.restaurants),
-        categories: convertCategoryApp(data.categories),
-        ingredients: convertIngredientApp(data.ingredients),
-      });
-    } catch (errorEffect) {
-      console.warn(errorEffect);
+        appData =
+            loading || !!error || !data
+                ? undefined
+                : ({
+                      beverages: convertSimpleModel(data.beverages),
+                      desserts: convertSimpleModel(data.desserts),
+                      salads: convertSaladApp(data.salads),
+                      sides: convertSideApp(data.sides),
+                      sandwiches: convertSandwichApp(data.sandwiches),
+                      burgers: convertBurgerApp(data.burgers),
+                      items: convertItemApp(data.items),
+                      options: convertOptionApp(data.options),
+                      restaurants: convertRestaurantApp(data.restaurants),
+                      categories: convertCategoryApp(data.categories),
+                      ingredients: convertIngredientApp(data.ingredients),
+                  } as AppData);
+    } catch (error) {
+        console.error(`Something went wrong: ${error}`);
+        throw error;
     }
-  }, [loading, error, data]);
 
-  return { loading, error, appData };
+    return { loading, error, appData };
 };
--- src/hooks/useRestaurantData.ts
+++ src/hooks/useRestaurantData.ts
@@ -2,84 +2,73 @@ import { useState, useCallback, useMemo } from "react";
 import { AppData, RestaurantData, RestaurantCtx } from "../types";
 
 import {
-  convertSimpleModel,
-  convertItem,
-  convertIngredient,
-  convertSidePage,
-  convertSaladPage,
-  convertSandwichPage,
-  convertBurgerPage,
+    convertSimpleModel,
+    convertItem,
+    convertIngredient,
+    convertSidePage,
+    convertSaladPage,
+    convertSandwichPage,
+    convertBurgerPage,
 } from "./Restaurants.functions";
 
 export const useRestaurantData = (appData: AppData) => {
-  const [restaurantData, setRestaurantData] = useState<RestaurantData>();
+    const [restaurantData, setRestaurantData] = useState<RestaurantData>();
 
-  const filterProductForResturant = useCallback(
-    (idRestaurant: string) => {
-      try {
-        const { restaurants } = appData;
-        const restaurant = restaurants.find(({ id }) => id === idRestaurant);
-        if (restaurant) {
-          const items = convertItem(appData.items, "1");
-          const beverages = convertSimpleModel(appData.beverages, idRestaurant);
-          const desserts = convertSimpleModel(appData.desserts, idRestaurant);
-          const ingredients = convertIngredient(
-            appData.ingredients,
-            idRestaurant
-          );
-          const sidesPages = convertSidePage(
-            appData.sides,
-            idRestaurant,
-            ingredients
-          );
-          const saladsPages = convertSaladPage(
-            appData.salads,
-            idRestaurant,
-            ingredients
-          );
-          const sandwichesPages = convertSandwichPage(
-            appData.sandwiches,
-            idRestaurant,
-            ingredients,
-            appData.options,
-            items,
-            sidesPages,
-            beverages
-          );
+    const filterProductForResturant = useCallback(
+        (idRestaurant: string) => {
+            try {
+                const { restaurants } = appData;
+                const restaurant = restaurants.find(({ id }) => id === idRestaurant);
+                if (restaurant) {
+                    const items = convertItem(appData.items, idRestaurant);
+                    const beverages = convertSimpleModel(appData.beverages, idRestaurant);
+                    const desserts = convertSimpleModel(appData.desserts, idRestaurant);
+                    const ingredients = convertIngredient(appData.ingredients, idRestaurant);
+                    const sidesPages = convertSidePage(appData.sides, idRestaurant, ingredients);
+                    const saladsPages = convertSaladPage(appData.salads, idRestaurant, ingredients);
+                    const sandwichesPages = convertSandwichPage(
+                        appData.sandwiches,
+                        idRestaurant,
+                        ingredients,
+                        appData.options,
+                        items,
+                        sidesPages,
+                        beverages
+                    );
 
-          const burgersPages = convertBurgerPage(
-            appData.burgers,
-            idRestaurant,
-            ingredients,
-            appData.options,
-            items,
-            sidesPages,
-            beverages
-          );
-          setRestaurantData({
-            items,
-            beverages,
-            desserts,
-            ingredients,
-            sidesPages,
-            saladsPages,
-            sandwichesPages,
-            burgersPages,
-          });
-        } else {
-          console.warn(`There is not restaurant with the id: ${idRestaurant}`);
-        }
-      } catch (error) {
-        console.warn(error);
-      }
-    },
-    [appData]
-  );
+                    const burgersPages = convertBurgerPage(
+                        appData.burgers,
+                        idRestaurant,
+                        ingredients,
+                        appData.options,
+                        items,
+                        sidesPages,
+                        beverages
+                    );
+                    setRestaurantData({
+                        items,
+                        beverages,
+                        desserts,
+                        ingredients,
+                        sidesPages,
+                        saladsPages,
+                        sandwichesPages,
+                        burgersPages,
+                    });
+                } else {
+                    console.warn(`There is not restaurant with the id: ${idRestaurant}`);
+                }
+            } catch (error) {
+                console.warn(error);
+            }
+        },
+        [appData]
+    );
 
-  const value = useMemo(
-    () => [restaurantData, filterProductForResturant] as RestaurantCtx,
-    [filterProductForResturant, restaurantData]
-  );
+    const value = useMemo(
+        () => [restaurantData, filterProductForResturant] as RestaurantCtx,
+        [filterProductForResturant, restaurantData]
+    );
 
-  return value;
+    return value;
 };
